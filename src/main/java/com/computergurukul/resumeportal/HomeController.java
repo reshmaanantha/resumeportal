b175a6801c4fd0b915ab7d5539b07eb2c811d12e
reshmaanantha <reshma84@gmail.com>
2021-07-03 16:57:32 +0530

    Added sample resume templates downloaded from internet

--- src/main/java/com/computergurukul/resumeportal/HomeController.java
+++ src/main/java/com/computergurukul/resumeportal/HomeController.java
@@ -1,14 +1,31 @@
 package com.computergurukul.resumeportal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class HomeController {
 	
 	@GetMapping("/")
 	public String home() {
 		return "Hello";
 	}
-
+	@GetMapping("/edit")
+	public String edit() {
+		return "edit pg";
+	}
+	
+	@GetMapping("/view/{userId}")
+	public String view(@PathVariable String userId,Model theModel) {
+		theModel.addAttribute("userId",userId);
+		if(userId.equals("1"))
+		return "profile-templates/1/index";
+		if(userId.equals("2"))
+			return "profile-templates/2/index";
+		if(userId.equals("3"))
+			return "profile-templates/3/index";
+	return null;	
+	}
 }
