3bd452d698e34d714e9398906ac3abb4d639bca9
reshmaanantha <reshma84@gmail.com>
2021-07-03 11:24:38 +0530

    Resume portal application

---
+++ src/main/java/com/computergurukul/resumeportal/ResumeportalApplication.java
@@ -0,0 +1,15 @@
+package com.computergurukul.resumeportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses=UserRepository.class)
+public class ResumeportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeportalApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/computergurukul/resumeportal/UserRepository.java
@@ -0,0 +1,12 @@
+package com.computergurukul.resumeportal;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.computergurukul.resumeportal.models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	Optional<User>findByUserName(String userName);
+
+}
