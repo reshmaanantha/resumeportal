2fa857a52b6eba0fa068cba90571a5f6f66472a6
reshmaanantha <reshma84@gmail.com>
2021-07-03 11:54:04 +0530

    Added userdetails

---
+++ src/main/java/com/computergurukul/resumeportal/MyUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.computergurukul.resumeportal;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.computergurukul.resumeportal.models.User;
+
+
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUserName(userName);
+
+        user.orElseThrow(() -> new UsernameNotFoundException("Not found: " + userName));
+
+        return user.map(MyUserDetails::new).get();
+    }
+}
